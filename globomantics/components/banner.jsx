5246db56e80daf137895c5bca489e6431128620f
Dylan <dylan.drumgoole@fenergo.com>
2024-04-17 16:09:22 +0100

    Adding bootstrap

--- globomantics/components/banner.jsx
+++ globomantics/components/banner.jsx
@@ -1,10 +1,10 @@
 const Banner = () => {
   return (
-    <header>
-      <div>
-        <img src="./next.svg" alt="logo" />
+    <header className="row mb-4">
+      <div className="col-5 mt-4">
+        <img src="./next.svg" alt="logo" className="logo" />
       </div>
-      <div>
+      <div className="col-7 mt-5">
         Providing houses all over the world
       </div>
     </header>
