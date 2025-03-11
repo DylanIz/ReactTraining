33079c85a294cd067573ffb0795fa94b22adec01
Dylan <dylan.drumgoole@fenergo.com>
2025-03-11 16:57:56 +0000

    Drawer

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import TempDrawer from "./Components/Drawer";
 
 type Props = {};
 
 const App = ({}: Props) => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <TempDrawer />
+    </>
+  );
 };
 
 export default App;
