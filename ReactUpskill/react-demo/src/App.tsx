80787d6c4562e61253f564215d216d3c0732c9f0
Dylan <dylan.drumgoole@fenergo.com>
2025-03-11 16:53:04 +0000

    Setup mui navbar

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 type Props = {};
 
-function App({}: Props) {
-  return <div>App</div>;
-}
+const App = ({}: Props) => {
+  return <Navbar />;
+};
 
 export default App;
