7dbc17e4d6d6fb358cbb667158ec084f001edf4d
Dylan <dylan.drumgoole@fenergo.com>
2025-03-13 17:32:22 +0000

    Footer

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,9 +1,13 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 type Props = {};
 
 const App = ({}: Props) => {
   return (
     <>
+      <Header />
+      <Footer />
     </>
   );
 };
---
+++ ReactUpskill/react-demo/src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="footer">
+      <p>© 2024 My Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
