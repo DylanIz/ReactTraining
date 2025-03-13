72c5b9df0c10c835f6722420728242acc7aa0567
Dylan <dylan.drumgoole@fenergo.com>
2025-03-13 20:31:10 +0000

    Update ProductInfo.tsx

--- ReactUpskill/react-demo/src/Components/ProductInfo.tsx
+++ ReactUpskill/react-demo/src/Components/ProductInfo.tsx
@@ -1,13 +1,15 @@
-const product = {
-  name: "laptop",
-  price: 100,
-  availability: "In Stock",
-};
-
 const ProductInfo = () => {
+  const product = {
+    name: "laptop",
+    price: 100,
+    availability: "In Stock",
+  };
   return (
     <div>
-      {product.name}, {product.price}, {product.availability}
+      <h1>Product Information</h1>
+      <h2>Name: {product.name}</h2>
+      <h2>Price: ${product.price}</h2>
+      <h2>Availability: {product.availability}</h2>
     </div>
   );
 };
