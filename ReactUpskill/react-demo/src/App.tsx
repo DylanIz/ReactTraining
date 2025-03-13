04c481c8170c62798e519fddb9927fd7fd569061
Dylan <dylan.drumgoole@fenergo.com>
2025-03-13 17:27:54 +0000

    Cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,14 +1,9 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import TempDrawer from "./Components/Drawer";
 
 type Props = {};
 
 const App = ({}: Props) => {
   return (
     <>
-      <Navbar />
-      <TempDrawer />
     </>
   );
 };
