a7cba3ef68df968ff3921def5296e92cb69892cb
Dylan <dylan.drumgoole@fenergo.com>
2025-03-18 17:57:26 +0000

    Button with state count

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+type ButtonProps = {
+  count: number;
+  onIncrement: () => void;
+};
+
+const Button = ({ count, onIncrement }: ButtonProps) => {
+  return <button onClick={onIncrement}>{count}</button>;
+};
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
     document.body.classList.add("dark-mode");
   }, []);
 
   return (
     <>
-      <div className="app-container"></div>
+      <div className="app-container">
+        <Button count={count} onIncrement={() => setCount(count + 1)} />
+      </div>
     </>
   );
 };
