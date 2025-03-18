4ea704f61a3487bf65cf7a3e46cf511212ed2b9b
Dylan <dylan.drumgoole@fenergo.com>
2025-03-18 17:14:24 +0000

    Cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
 import "./App.css";
-import Weather from "./Components/Weather";
-import UserStatus from "./Components/UserStatus";
-import Greeting from "./Components/Greeting";
 
 const App = () => {
   useEffect(() => {
@@ -11,20 +8,7 @@ const App = () => {
 
   return (
     <>
-      <div className="app-container">
-        <h1>Conditional Rendering</h1>
-        {/* Weather */}
-        <Weather temperature={10} />
-        <Weather temperature={20} />
-        <Weather temperature={30} />
-        {/* UserStatus */}
-        <UserStatus loggedIn={true} isAdmin={true} />
-        <UserStatus loggedIn={true} isAdmin={false} />
-        <UserStatus loggedIn={false} isAdmin={false} />
-        {/* Greeting */}
-        <Greeting timeOfDay="morning" />
-        <Greeting timeOfDay="afternoon" />
-      </div>
+      <div className="app-container"></div>
     </>
   );
 };
