7c0ddafb2d9453b598a90488523b4b087e965c01
Dylan <dylan.drumgoole@fenergo.com>
2025-03-18 16:43:57 +0000

    Cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,19 +1,12 @@
 import { useEffect } from "react";
 import "./App.css";
-import Person from "./Components/Person";
-import Products from "./Components/Products";
 
 const App = () => {
   useEffect(() => {
     document.body.classList.add("dark-mode");
   }, []);
 
-  return (
-    <div className="app-container">
-      <Person name="Dylan" age={27}></Person>
-      <Products name="Laptop" price={1000}></Products>
-    </div>
-  );
+  return <div className="app-container"></div>;
 };
 
 export default App;
