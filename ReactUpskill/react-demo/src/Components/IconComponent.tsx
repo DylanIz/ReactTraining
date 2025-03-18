79a7e23e08f4200087b5b193760a2f4d60f7c230
Dylan <dylan.drumgoole@fenergo.com>
2025-03-18 17:45:50 +0000

    Update IconComponent.tsx

--- ReactUpskill/react-demo/src/Components/IconComponent.tsx
+++ ReactUpskill/react-demo/src/Components/IconComponent.tsx
@@ -7,7 +7,15 @@ type Props = {
 
 const IconComponent = ({ icon }: Props) => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: "lightgreen",
+        padding: "1rem",
+        borderRadius: "10px",
+        color: "darkgreen",
+        fontSize: "30px",
+      }}
+    >
       <h1>IconComponent</h1>
       <h2>{React.createElement(icon)}</h2>
     </div>
