ca74d22eaafd4bd95bdd2189f4225e02b9c1b36c
Dylan <dylan.drumgoole@fenergo.com>
2025-03-21 15:21:17 +0000

    Update useEffect to remove darkmode

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,14 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [removeDarkMode, setRemoveDarkMode] = useState(false);
+
   useEffect(() => {
     document.body.classList.add("dark-mode");
-  }, []);
+    if (removeDarkMode) {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [removeDarkMode]);
 
   return (
     <>
       <div className="app-container">
+        <button
+          onClick={() => setRemoveDarkMode(true)}
+          style={{
+            position: "absolute",
+            bottom: "10px",
+            left: "10px",
+          }}
+        >
+          RemoveDarkmode
+        </button>
       </div>
     </>
   );
