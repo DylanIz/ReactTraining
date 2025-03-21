90332f584614adf6a88626ce5da3d24ac6a7404d
Dylan <dylan.drumgoole@fenergo.com>
2025-03-21 15:23:55 +0000

    Refactor button to toggle

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -2,27 +2,28 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [removeDarkMode, setRemoveDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    document.body.classList.add("dark-mode");
-    if (removeDarkMode) {
+    if (darkMode) {
       document.body.classList.remove("dark-mode");
+    } else {
+      document.body.classList.add("dark-mode");
     }
-  }, [removeDarkMode]);
+  }, [darkMode]);
 
   return (
     <>
       <div className="app-container">
         <button
-          onClick={() => setRemoveDarkMode(true)}
+          onClick={() => setDarkMode((prev) => !prev)}
           style={{
             position: "absolute",
             bottom: "10px",
             left: "10px",
           }}
         >
-          RemoveDarkmode
+          {darkMode ? "Enable Dark Mode" : "Disable Dark Mode"}
         </button>
       </div>
     </>
