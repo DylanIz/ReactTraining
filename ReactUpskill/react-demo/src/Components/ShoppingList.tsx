9c38b71c3827609b8b31ff6326e26b20a0ab4a4e
Dylan <dylan.drumgoole@fenergo.com>
2025-03-21 14:31:21 +0000

    Refactor

--- ReactUpskill/react-demo/src/Components/ShoppingList.tsx
+++ ReactUpskill/react-demo/src/Components/ShoppingList.tsx
@@ -3,42 +3,45 @@ import React, { useState } from "react";
 type Props = {};
 
 const ShoppingList = (props: Props) => {
-  const [shoppingList, setShoppingList] = useState<
-    { name: string; quantity: number }[]
-  >([]);
+  const [items, setItems] = useState<{ name: string; quantity: string }[]>([]);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const { name, value } = e.target;
-
-    setShoppingList((prevShoppingList) => ({
-      ...prevShoppingList,
-      [name]: value,
-    }));
+    setItems((prevItems) => [...prevItems, { name, quantity }]);
+    setName("");
+    setQuantity("");
   };
 
   return (
     <div>
       <h1>Shopping List</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
-          placeholder="Name"
-          value={shoppingList.name}
-          onChange={handleChange}
+          placeholder="Item Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         ></input>
         <input
           type="number"
           name="quantity"
           placeholder="Quantity"
-          value={shoppingList.quantity}
-          onChange={handleChange}
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
         ></input>
+        <button type="submit">Add Item</button>
       </form>
 
-      <h1>Item Name:{shoppingList.name}</h1>
-      <h1>Item Quantity:{shoppingList.quantity}</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            Item Name{item.name} Item Quantity{item.quantity}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
