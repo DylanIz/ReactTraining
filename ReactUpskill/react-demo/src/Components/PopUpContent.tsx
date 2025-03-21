62dc74847d9f99ed1fa673be55cd89775abc1692
Dylan <dylan.drumgoole@fenergo.com>
2025-03-21 14:57:30 +0000

    React Portal

--- ReactUpskill/react-demo/src/Components/PopUpContent.tsx
+++ ReactUpskill/react-demo/src/Components/PopUpContent.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import { createPortal } from "react-dom";
 
 type Props = {
   copied: boolean;
 };
 
 const PopUpContent = ({ copied }: Props) => {
-  return (
+  return createPortal(
     <section style={{ position: "absolute", bottom: "3rem" }}>
       {copied && <div>Copied to clipboard.</div>}
-    </section>
+    </section>,
+    document.querySelector("#popup-content") as HTMLElement
   );
 };
 
