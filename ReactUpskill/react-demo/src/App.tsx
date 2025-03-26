736639afe8a98cb5d07f13b36337d709255fcf5e
Dylan <dylan.drumgoole@fenergo.com>
2025-03-26 18:52:28 +0000

    cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,40 +1,11 @@
-import { useEffect, useState } from "react";
+import { DarkMode } from "@mui/icons-material";
 import "./App.css";
-import DarkMode from "./Components/DarkMode";
-
-type Todo = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
 
 const App = () => {
-  const [data, setData] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos"
-      );
-      const data = await response.json();
-      if (data && data.length) {
-        setData(data);
-      }
-    }
-    getData();
-  }, [data]);
-
   return (
     <>
-      <div className="app-container">
-        <DarkMode />
-        <ul>
-          {data.map((todo: Todo) => (
-            <li key={todo.id}>{todo.title}</li>
-          ))}
-        </ul>
-      </div>
+      <div className="app-container"></div>
+      <DarkMode />
     </>
   );
 };
