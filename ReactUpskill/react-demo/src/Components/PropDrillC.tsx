9963fa05061dc178cae8b646b133c6fe5a408ba8
Dylan <dylan.drumgoole@fenergo.com>
2025-03-26 20:34:19 +0000

    Refactor from Context.Provider to useContext hook

--- ReactUpskill/react-demo/src/Components/PropDrillC.tsx
+++ ReactUpskill/react-demo/src/Components/PropDrillC.tsx
@@ -1,24 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Data } from "../App";
 import { Data1 } from "../App";
 
 const PropDrillC = () => {
+  const name = useContext(Data);
+  const age = useContext(Data1);
   return (
-    <Data.Consumer>
-      {(name) => {
-        return (
-          <Data1.Consumer>
-            {(age) => {
-              return (
-                <h1>
-                  My name is {name} and my age is {age}
-                </h1>
-              );
-            }}
-          </Data1.Consumer>
-        );
-      }}
-    </Data.Consumer>
+    <h1>
+      My name is {name} and my age is {age}
+    </h1>
   );
 };
 
