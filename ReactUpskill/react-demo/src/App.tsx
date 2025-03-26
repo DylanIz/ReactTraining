7aac2d94415ddbe880f661fb60536f4e3015fcee
Dylan <dylan.drumgoole@fenergo.com>
2025-03-26 20:29:03 +0000

    Passing multiple contexts

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -4,16 +4,20 @@ import DarkMode from "./Components/DarkMode";
 import PropDrillA from "./Components/PropDrillA";
 
 export const Data = createContext("John Doe");
+export const Data1 = createContext(99);
 
 const App = () => {
   const name = "Dylan";
+  const age = 27;
   return (
     <>
       <Data.Provider value={name}>
+        <Data1.Provider value={age}>
         <div className="app-container">
           <PropDrillA />
           <DarkMode />
         </div>
+        </Data1.Provider>
       </Data.Provider>
     </>
   );
--- ReactUpskill/react-demo/src/Components/PropDrillC.tsx
+++ ReactUpskill/react-demo/src/Components/PropDrillC.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Data } from "../App";
+import { Data1 } from "../App";
 
 const PropDrillC = () => {
   return (
     <Data.Consumer>
       {(name) => {
-        return <h1>My name is {name}</h1>;
+        return (
+          <Data1.Consumer>
+            {(age) => {
+              return (
+                <h1>
+                  My name is {name} and my age is {age}
+                </h1>
+              );
+            }}
+          </Data1.Consumer>
+        );
       }}
     </Data.Consumer>
   );
