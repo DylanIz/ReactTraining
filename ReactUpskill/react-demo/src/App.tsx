bb5d5b8d9c8c9d57e4b368db9cdf6706d4fd0215
Dylan <dylan.drumgoole@fenergo.com>
2025-03-26 18:42:26 +0000

    UseEffect basic fetch

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const data = await response.json();
+      if (data && data.length) {
+        setData(data);
+      }
+    }
+  }, [data]);
+
   return (
     <>
       <div className="app-container">
