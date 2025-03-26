3bc700beffda67630e4e614fdfa01af7cf238306
Dylan <dylan.drumgoole@fenergo.com>
2025-03-26 19:17:40 +0000

    cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import BasicEffect from "./Components/BasicEffect";
-import CounterEffect from "./Components/CounterEffect";
 import DarkMode from "./Components/DarkMode";
-import FetchDataEffect from "./Components/FetchDataEffect";
 
 const App = () => {
   return (
     <>
       <div className="app-container">
-        <BasicEffect />
-        <CounterEffect />
-        <FetchDataEffect />
         <DarkMode />
       </div>
     </>
