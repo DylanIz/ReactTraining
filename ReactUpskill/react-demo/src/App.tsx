57c911d028d9828267a034f609ec16e652f1ce85
DylanIz <dylandrumgoole2@hotmail.com>
2025-03-27 20:23:07 +0000

    Cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,24 +1,12 @@
-import { createContext } from "react";
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
-import PropDrillA from "./Components/PropDrillA";
-
-export const Data = createContext("John Doe");
-export const Data1 = createContext(99);
 
 const App = () => {
-  const name = "Dylan";
-  const age = 27;
   return (
     <>
-      <Data.Provider value={name}>
-        <Data1.Provider value={age}>
         <div className="app-container">
-          <PropDrillA />
           <DarkMode />
         </div>
-        </Data1.Provider>
-      </Data.Provider>
     </>
   );
 };
