a8a46c93c5ab77a85bb46767b0bc28fe5503735a
DylanIz <dylandrumgoole2@hotmail.com>
2025-04-02 21:53:31 +0100

    useReducer setup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,11 +1,36 @@
+import { useReducer } from "react";
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
 
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state: { count: number }, action: { type: string }) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
       <div className="app-container">
         <DarkMode />
+        <button onClick={() => dispatch({ type: "increment" })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          Decrement
+        </button>
+        <p>Count: {state.count}</p>
       </div>
     </>
   );
