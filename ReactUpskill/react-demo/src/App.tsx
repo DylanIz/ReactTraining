339fb4e7fec4c526e5bdc91aa50af0f7e6bab1d5
DylanIz <dylandrumgoole2@hotmail.com>
2025-04-02 21:37:56 +0100

    clean up

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,18 +1,11 @@
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
-import UpdateUser from "./Components/UpdateUser";
-import UserProfile from "./Components/UserProfile";
-import { UserProvider } from "./UserContext";
 
 const App = () => {
   return (
     <>
       <div className="app-container">
-        <UserProvider>
-          <DarkMode />
-          <UserProfile />
-          <UpdateUser />
-        </UserProvider>
+        <DarkMode />
       </div>
     </>
   );
