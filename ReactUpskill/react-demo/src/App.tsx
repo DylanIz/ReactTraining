853ba7aba6919ea5fd9d04aad99d5b8fb0d4bc27
Dylan <dylan.drumgoole@fenergo.com>
2025-04-04 17:13:04 +0100

    initial setup before custom hook refactor

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
 
+type Data = {
+  id: number;
+  title: string;
+  body: string;
+};
+
 const App = () => {
+  const [data, setData] = useState<Data[]>([]);
+
+  const getData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div className="app-container">
+        {data &&
+          data.map((item: Data) => (
+            <div key={item.id}>
+              <h1>{item.title}</h1>
+            </div>
+          ))}
         <DarkMode />
       </div>
     </>
