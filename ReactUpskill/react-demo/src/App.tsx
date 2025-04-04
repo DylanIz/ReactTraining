747f1ad90e86b68085ab81045cf7a5913bb5939b
Dylan <dylan.drumgoole@fenergo.com>
2025-04-04 15:38:06 +0100

    use ref basic example

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,10 +1,20 @@
+import { useRef } from "react";
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
 
 const App = () => {
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  const handleFocus = () => {
+    inputElement.current?.focus();
+    inputElement.current!.value = "Dylan";
+  };
+
   return (
     <>
       <div className="app-container">
+        <input type="text" ref={inputElement} />
+        <button onClick={handleFocus}>Focus</button>
         <DarkMode />
       </div>
     </>
