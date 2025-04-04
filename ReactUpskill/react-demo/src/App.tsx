8fc0d2f59e5a542daaa6f075f0b29932b0b1c2a0
Dylan <dylan.drumgoole@fenergo.com>
2025-04-04 17:47:13 +0100

    cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
-import UniqueId from "./Components/UniqueId";
 
 const App = () => {
   return (
     <>
       <div className="app-container">
         <DarkMode />
-        <UniqueId />
       </div>
     </>
   );
