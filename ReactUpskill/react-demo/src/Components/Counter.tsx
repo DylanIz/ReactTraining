90d25cb3cdbaebcb0309e1fc896a329ad9a8c780
Dylan <dylan.drumgoole@fenergo.com>
2025-04-04 15:19:50 +0100

    Refactor counter to handle payloa

--- ReactUpskill/react-demo/src/Components/Counter.tsx
+++ ReactUpskill/react-demo/src/Components/Counter.tsx
@@ -1,28 +1,54 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const initialState = {
   count: 0,
 };
 
-const reducer = (state: { count: number }, action: { type: string }) => {
+type Action =
+  | { type: "increment" | "decrement" | "reset" }
+  | { type: "incrementByAmount" | "decrementByAmount"; payload: number };
+
+const reducer = (state: { count: number }, action: Action) => {
   switch (action.type) {
     case "increment":
       return { ...state, count: state.count + 1 };
     case "decrement":
       return { ...state, count: state.count - 1 };
-    default:
+    case "incrementByAmount":
+      return { ...state, count: state.count + action.payload };
+    case "decrementByAmount":
+      return { ...state, count: state.count - action.payload };
+    case "reset":
       return initialState;
+    default:
+      return state;
   }
 };
 
 const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [input, setInput] = useState(0);
+
+  const handleIncrementByAmount = () => {
+    dispatch({ type: "incrementByAmount", payload: input });
+  };
+
+  const handleDecrementByAmount = () => {
+    dispatch({ type: "decrementByAmount", payload: input });
+  };
 
   return (
     <div>
       <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
       <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(parseInt(e.target.value))}
+      />
+      <button onClick={handleIncrementByAmount}>Add</button>
+      <button onClick={handleDecrementByAmount}>Subtract</button>
       <p>Count: {state.count}</p>
     </div>
   );
