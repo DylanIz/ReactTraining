1aaddfaaa5ecd2ef334f11e7952705a2be3fade3
Dylan <dylan.drumgoole@fenergo.com>
2025-04-04 17:07:10 +0100

    cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
-import FocusInput from "./Components/FocusInput";
-import Timer from "./Components/Timer";
 
 const App = () => {
   return (
     <>
       <div className="app-container">
-        <FocusInput />
         <DarkMode />
-        <Timer />
       </div>
     </>
   );
