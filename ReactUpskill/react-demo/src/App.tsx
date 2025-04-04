efd5d7dc5b0856c90c129786ae45285b6a9ec0ce
Dylan <dylan.drumgoole@fenergo.com>
2025-04-04 15:25:15 +0100

    Cleanup

--- ReactUpskill/react-demo/src/App.tsx
+++ ReactUpskill/react-demo/src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import DarkMode from "./Components/DarkMode";
-import Counter from "./Components/Counter";
 
 const App = () => {
   return (
     <>
       <div className="app-container">
         <DarkMode />
-        <Counter />
       </div>
     </>
   );
