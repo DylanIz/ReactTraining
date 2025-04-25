eb7a50a043a0bdda3ee1f764bfed445cab947ca7
Dylan <dylan.drumgoole@fenergo.com>
2025-04-25 14:45:51 +0100

    Accordion

--- ReactProjects/project-demos/src/App.jsx
+++ ReactProjects/project-demos/src/App.jsx
@@ -1,5 +1,16 @@
+import Accordion from "./components/Accordion";
+import { accordionData } from "./utils/content";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
