0bf64a5afb5e66524e54c34f3d7aec4600a6a100
Dylan <dylan.drumgoole@fenergo.com>
2025-04-25 16:02:02 +0100

    Nav setup

--- ReactProjects/project-shop/shop/src/App.jsx
+++ ReactProjects/project-shop/shop/src/App.jsx
@@ -1,12 +1,22 @@
 import Nav from "./Nav/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import Price from "./Sidebar/Price/Price";
+import Category from "./Sidebar/Category/Category";
+import Colours from "./Sidebar/Colours/Colours";
+
 const App = () => {
   return (
     <>
       <Nav />
       <Products />
       <Recommended />
+      <Sidebar>
+        <Price />
+        <Category />
+        <Colours />
+      </Sidebar>
     </>
   );
 };
