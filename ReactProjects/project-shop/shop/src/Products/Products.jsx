8e0739ec8e5282cc2cbbe288d24d19425c055c26
Dylan <dylan.drumgoole@fenergo.com>
2025-04-25 16:09:49 +0100

    Products setup

--- ReactProjects/project-shop/shop/src/Products/Products.jsx
+++ ReactProjects/project-shop/shop/src/Products/Products.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import "./Products.css";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <section className="card-container">
+      <section className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt="Shoe"
+        />
+        <div className="card-details">
+          <h3 className="card-title">Nike Air Monarch IV</h3>
+          <section className="card-reviews">
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <span className="total-reviews">123 reviews</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$199.00</del>
+              <span>$100.00</span>
+            </div>
+
+            <div className="bag">
+              <BsFillBagHeartFill />
+            </div>
+          </section>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default Products;
