5db1501ffcfb24b8831414f708dfe7e90d945a58
Dylan <dylan.drumgoole@fenergo.com>
2025-04-25 17:02:53 +0100

    Begin refactor

--- ReactProjects/project-shop/shop/src/components/Card.jsx
+++ ReactProjects/project-shop/shop/src/components/Card.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <section className="card">
+      <img
+        src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+        alt="Shoe"
+        className="card-img"
+      />
+      <div className="card-details">
+        <h3 className="card-title">Nike Air Monarch IV</h3>
+        <section className="card-reviews">
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <span className="total-reviews">123 reviews</span>
+        </section>
+        <section className="card-price">
+          <div className="price">
+            <del>$199.00</del>
+            <span>$100.00</span>
+          </div>
+
+          <div className="bag">
+            <BsFillBagHeartFill className="bag-icon" />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
 };
 
 export default Card;
