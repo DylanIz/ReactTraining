8e58ea71f7fca1dcd9494f1e6c7ab8292ecc5b6d
Dylan <dylan.drumgoole@fenergo.com>
2025-04-25 17:00:03 +0100

    Improve sidebar

--- ReactProjects/project-shop/shop/src/Sidebar/Colours/Colours.jsx
+++ ReactProjects/project-shop/shop/src/Sidebar/Colours/Colours.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import "./Colours.css";
 
 const Colours = () => {
-  return <div>Colours</div>;
+  return (
+    <div className="ml">
+      <label className="sidebar-label-container color-title">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>
+        Black
+      </label>
+
+      <label className="sidebar-label-container color-title">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>
+        Blue
+      </label>
+
+      <label className="sidebar-label-container color-title">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>
+        Red
+      </label>
+    </div>
+  );
 };
 
 export default Colours;
