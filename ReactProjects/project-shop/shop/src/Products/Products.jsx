872ec301dbac45ddb9ff2fc574a4e44b013d6ac0
Dylan <dylan.drumgoole@fenergo.com>
2025-05-01 16:21:23 +0100

    Remove ESLint configuration file and update product data import in Products component. Add new data file for product listings with detailed information.

--- ReactProjects/project-shop/shop/src/Products/Products.jsx
+++ ReactProjects/project-shop/shop/src/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Products.css";
 import Card from "../components/Card";
-import { data } from "../db/data";
+import data from "../db/data.jsx";
 
 const Products = () => {
   return (
