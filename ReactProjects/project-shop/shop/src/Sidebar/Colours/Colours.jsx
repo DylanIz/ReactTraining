8121a6b74d3de36f60adb9d4b0077d8bbb0fca72
Dylan <dylan.drumgoole@fenergo.com>
2025-05-01 15:44:39 +0100

    f

--- ReactProjects/project-shop/shop/src/Sidebar/Colours/Colours.jsx
+++ ReactProjects/project-shop/shop/src/Sidebar/Colours/Colours.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Colours.css";
 import Input from "../../components/Input";
+
 const Colours = () => {
   return (
     <div className="ml">
+      <div className="sidebar-title color-title">
+        <h2>Colours</h2>
+      </div>
       <Input title="Black" />
       <Input title="Blue" />
       <Input title="Red" />
