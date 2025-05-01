215c55b6e6fb2269b7b87a2ed63f8aec29d2d5a4
Dylan <dylan.drumgoole@fenergo.com>
2025-05-01 15:47:09 +0100

    f

--- ReactProjects/project-shop/shop/src/Products/Products.jsx
+++ ReactProjects/project-shop/shop/src/Products/Products.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Products.css";
 import Card from "../components/Card";
-import data from "../db/data";
+import { data } from "../db/data";
 
 const Products = () => {
   return (
     <section className="card-container">
-      {data.map((item) => (
-        <Card key={item.id} {...item} />
+      {data.map((item, index) => (
+        <Card key={index} {...item} />
       ))}
     </section>
   );
